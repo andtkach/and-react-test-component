9c8974e01fb28f3c754e1928e57cb98164c97005
Andrii Tkach <andtkach@gmail.com>
2023-06-22 18:15:57 +0200

    Version update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 npm login
 
-npm run publish:npm
+npm run publish-win:npm
+        publish-lin:npm
 
 npm publish --dry-run
 
---
+++ dist/DemoComponent.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const DemoComponent = () => {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, "Demo Component"));
+};
